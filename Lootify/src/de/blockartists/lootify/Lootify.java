3cb5598f20b7f06d2a1d6300b57e53fcc7f850af
Richard <Richard@DESKTOP-IICFMLH.Speedport_W_724V_Typ_A_05011603_05_028>
2018-08-06 20:25:46 +0200

    Initial commit

---
+++ Lootify/src/de/blockartists/lootify/Lootify.java
@@ -0,0 +1,17 @@
+package de.blockartists.lootify;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lootify extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
